31df08db14af941f828b458bd3cc9b9a24e68321
Z.Y <441669631@qq.com>
2024-03-17 08:18:01 +0800

    Update Learn_realdata.sh

--- scripts/Learn_realdata.sh
+++ scripts/Learn_realdata.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../build/ode_treefile --DataT="true_data/data_t.txt" --DataX="true_data/data_x.txt" --TreeFile="example4.json" --ConstantUpperBound=30 --ConstantLowerBound=0 --MaxEpoch=5000 --WhenPenalty=1 --PreTraining=200 --OperatorSetting="op.ini" --Epsilon=0.2
+../build/ode_treefile --DataT="real_data/data_t.txt" --DataX="real_data/data_x.txt" --TreeFile="example4.json" --ConstantUpperBound=30 --ConstantLowerBound=0 --MaxEpoch=5000 --WhenPenalty=1 --PreTraining=200 --OperatorSetting="op.ini" --Epsilon=0.2
